eee0dfd0ce31c75edbd389108251bce924ea7403
Joeken10 <joekentafir@gmail.com>
2024-08-27 20:33:41 +0300

    add files

--- lib/functions.py
+++ lib/functions.py
@@ -5,10 +5,10 @@ def greet_programmer():
 
 
 def greet(name):
-    print(f"Hello, {name}!")
+    print(f"Hello Dear, {name}!")
 
 def greet_with_default(name="programmer"):
-   print(f"Hello, {name}!")  
+   print(f"Hello Mr.Bob, {name}!")  
 
 def add(num1, num2):
     return num1 + num2
@@ -16,3 +16,10 @@ def add(num1, num2):
 def halve(number):
     return number/2
 
+
+greet_programmer()
+greet("Liz")
+greet_with_default()
+greet_with_default("Charlton")
+sum_value = add(6, 12)
+half_value = halve(20)
